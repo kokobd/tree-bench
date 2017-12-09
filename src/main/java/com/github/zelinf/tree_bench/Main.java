001b3a2367d5f4beea09fde55583c83cd9d638c8
Zelin Feng <fengzlin@mail2.sysu.edu.cn>
2017-12-09 20:16:47 +0800

    Prepare GUI

--- src/main/java/com/github/zelinf/tree_bench/Main.java
+++ src/main/java/com/github/zelinf/tree_bench/Main.java
@@ -1,7 +1,14 @@
 package com.github.zelinf.tree_bench;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -11,6 +18,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.show();
+        primaryStage.setTitle("Main Application");
+        FXMLLoader loader = new FXMLLoader();
+        try {
+            Parent rootPane = loader.load(Main.class.getResourceAsStream("view/MainView.fxml"));
+
+            Scene scene = new Scene(rootPane);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
